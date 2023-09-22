106f81726cd67f429c2fb3b91febdac1eec1bfe5
Teddy Dumélé <teddy.dumele@gmail.com>
2023-09-22 22:50:36 +0200

    feat: adding some UI

--- src/pages/ChangePasswordPage/components/ChangePassword/ChangePassword.jsx
+++ src/pages/ChangePasswordPage/components/ChangePassword/ChangePassword.jsx
@@ -1,10 +1,11 @@
 import { useRef, useState } from "react";
 import { _isValidPassword } from "./validators";
-import { _DEFAULT_CONFIRMATION_PWD_WORDING, _DEFAULT_NEW_PWD_WORDING, _DEFAULT_RESTRICTED_MODEL, _DEFAULT_TITLE } from "./Constantes";
+import { _DEFAULT_CONFIRMATION_PWD_WORDING, _DEFAULT_ERROR_PWD_PATTERN, _DEFAULT_NEW_PWD_WORDING, _DEFAULT_RESTRICTED_MODEL, _DEFAULT_TITLE } from "./Constantes";
 
-export default function ChangePassword({ title, newPassword, confirmedPassword, validate = _isValidPassword, restrictedModel }) {
+export default function ChangePassword({ validate = _isValidPassword, restrictedModel, title, newPassword, confirmedPassword, errorMessage}) {
     const [passwordChanged, setPasswordChanged] = useState(false);
     const [disableButton, setDisableButton] = useState(true);
+    const [error, setError] = useState(false);
 
     const passwordRef = useRef("");
     const confirmedPasswordRef = useRef("");
@@ -12,23 +13,23 @@ export default function ChangePassword({ title, newPassword, confirmedPassword, 
     const processValidationPwd = (event) => {
         event.preventDefault();
 
-        if (password.current.value !== confirmedPassword.current.value) {
+        if (passwordRef.current.value !== confirmedPasswordRef.current.value) {
             console.log("Different passwords");
         }   
 
-        const valid = validate(password.current.value, restrictedModel);
+        const valid = validate(passwordRef.current.value, restrictedModel);
         if (valid) {
             setPasswordChanged(true);
         } else {
-            // TODO show something, think UX
-            console.log("Password doesn't respect the rules");
+            setError(true);
         }
     }
 
     const handleInputChange = () => {
-        const pass = password.current.value;
-        const conf = confirmedPassword.current.value;
+        const pass = passwordRef.current.value;
+        const conf = confirmedPasswordRef.current.value;
         setDisableButton(pass !== conf);
+        setError(false);
       };
 
     return (
@@ -49,13 +50,16 @@ export default function ChangePassword({ title, newPassword, confirmedPassword, 
                 <div className="input-group">
                     <label className="inputLabel" htmlFor="confirmPassword">{confirmedPassword ?? _DEFAULT_CONFIRMATION_PWD_WORDING}</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" ref={confirmedPasswordRef} onChange={handleInputChange}/>
+                    {error && <span className="error">{errorMessage ?? _DEFAULT_ERROR_PWD_PATTERN}</span>}
                 </div>
+                
 
                 <div className="buttonWrapper">
                     <button disabled={disableButton} type="submit" id="submitButton" className="submitButton pure-button pure-button-primary">
                         <span>Continue</span>
                     </button>
                 </div>
+                
             </form>
             {passwordChanged ? <p>Password Changed</p> : null}
         </div>
--- src/pages/ChangePasswordPage/components/ChangePassword/Constantes.js
+++ src/pages/ChangePasswordPage/components/ChangePassword/Constantes.js
@@ -3,3 +3,4 @@ export const _DEFAULT_RESTRICTED_MODEL = ["password", "qwerty", "1234", "qsdf"];
 export const _DEFAULT_TITLE = "Change Password";
 export const _DEFAULT_NEW_PWD_WORDING = "New Password";
 export const _DEFAULT_CONFIRMATION_PWD_WORDING = "Confirm Password";
+export const _DEFAULT_ERROR_PWD_PATTERN = "Password does not meet security requirements"
